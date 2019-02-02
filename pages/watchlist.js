c1cb363e30ca92e51c9709d6ae8c30cf7f0a3a41
leonerath <leon-erath@hotmail.de>
2019-02-02 15:57:30 +0100

    fixed ProjectHeader Bug in Watchlist.js

--- pages/watchlist.js
+++ pages/watchlist.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import ProjectHeader from "../components/Header/ProjectHeader.js";
 import WatchlistCover from "../components/Watchlist/WatchlstCover";
 
 class Watchlist extends Component {
@@ -7,23 +6,6 @@ class Watchlist extends Component {
     return (
       <div>
         <WatchlistCover />
-        <ProjectHeader
-          information={[
-            {
-              title: "TECHNOLOGY",
-              text: "Android App in Kotlin"
-            },
-            {
-              title: "GOAL",
-              text:
-                "See upcoming Movies and save them into your Watchlist to watch them later."
-            },
-            {
-              title: "DURATION",
-              text: "1 month"
-            }
-          ]}
-        />
       </div>
     );
   }
