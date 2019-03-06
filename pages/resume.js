9cc5138925c0d8519cbaea2031098ef6b4a94d38
Leon Erath <leon-erath@hotmail.de>
2019-03-06 16:49:37 +0100

    Update resume.js

--- pages/resume.js
+++ pages/resume.js
@@ -19,7 +19,7 @@ const Div2 = styled.div`
   flex-wrap: wrap;
   justify-content: center;
   align-items: center;
-  border-radius: 10px;
+  border-radius: 4px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   padding: 10px;
 `;
