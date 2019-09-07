69b6412362e2db7fa55081191c6224e9edb08eb9
Leon Erath <leon-erath@hotmail.de>
2019-09-07 12:59:37 +0200

    change

--- pages/_document.js
+++ pages/_document.js
@@ -2,24 +2,30 @@ import Document, { Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    const sheet = new ServerStyleSheet();
-    const page = renderPage(App => props =>
-      sheet.collectStyles(<App {...props} />)
-    );
-    const styleTags = sheet.getStyleElement();
-    return { ...page, styleTags };
-  }
+	static getInitialProps({ renderPage }) {
+		const sheet = new ServerStyleSheet();
+		const page = renderPage(App => props =>
+			sheet.collectStyles(<App {...props} />)
+		);
+		const styleTags = sheet.getStyleElement();
+		return { ...page, styleTags };
+	}
 
-  render() {
-    return (
-      <html>
-        <Head>{this.props.styleTags}</Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    );
-  }
+	render() {
+		return (
+			<html>
+				<Head>
+					{this.props.styleTags}
+					<meta
+						name="google-site-verification"
+						content="MSkqJgiVlLnyfRfjieCXuB1EKHUS_NtulUjFxuE2dQQ"
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</html>
+		);
+	}
 }
