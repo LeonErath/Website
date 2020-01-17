839992982a7cfefa2d04ab91b7c0d7cd06c2f105
Leon Erath <leon-erath@hotmail.de>
2020-01-17 21:25:41 +0100

    tests..

--- pages/index.js
+++ pages/index.js
@@ -122,7 +122,7 @@ const App = () => (
 			<LandingPage>
 				<TextContainer>
 					<h1>Hey, I’m Leon 👋</h1>
-					<h2>I’m a Fullstack Developer based in Düsseldorf.</h2>
+					<h2>I’m a Fullstack Developer based in Düsseldorf, Germany.</h2>
 				</TextContainer>
 				<ImageContainer>
 					<img src="/profile.png"></img>
